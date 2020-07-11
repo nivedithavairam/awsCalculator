f30b7e7b67f10567c4ff2059fd53194dec55351f
pbeniwal <pbeniwal@yahoo.com>
2020-07-11 12:46:31 +0530

    Update deploy.pp

--- puppet/deploy.pp
+++ puppet/deploy.pp
@@ -1,36 +1,36 @@
 class dockerwar::deploy {
 
-    file {'/home/devops/docker':
+    file {'/home/beniwal/docker':
             ensure => directory,
             mode => '0777'
     }
 
-    file {'/home/devops/docker/target':
+    file {'/home/beniwal/docker/target':
             ensure => directory,
             mode => '0777'
     }
 
-    file {'/home/devops/docker/target/calculator.war':
+    file {'/home/beniwal/docker/target/calculator.war':
             mode => '0777',
             source => "puppet:///modules/dockerwar/calculator.war"
     }
 
-    file {'/home/devops/docker/Dockerfile':
+    file {'/home/beniwal/docker/Dockerfile':
             mode => '0777',
             source => "puppet:///modules/dockerwar/Dockerfile"
     }
     exec {'Docker build':
-            cwd => '/home/devops/docker/',
+            cwd => '/home/beniwal/docker/',
             command => '/usr/bin/docker build -t calculatorwithwar .'
     }
 
     exec {'Docker rm':
-            cwd => '/home/devops/docker/',
+            cwd => '/home/beniwal/docker/',
             command => '/usr/bin/docker rm --force calculator8888 || echo'
     }
 
     exec {'Docker run':
-            cwd => '/home/devops/docker/',
+            cwd => '/home/beniwal/docker/',
             command => '/usr/bin/docker run -d -p 8888:8080 --name=calculator8888 calculatorwithwar'
     }
 }
